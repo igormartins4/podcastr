2f0f6bd2f39ebfc0b2fe5faa76b7274a8681dc4d
Igor Martins <iguinhoigor4@gmail.com>
2021-04-20 20:40:50 -0300

    feat: Mudar arquivos para a chegada do o componente Player

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
+import { Header } from '../components/Header'
+
 import '../styles/global.scss'
 
+import styles from '../styles/app.module.scss';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.wrapper}>
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
