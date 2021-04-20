1422bf6bb7c4bc3fbad961d04d1856426e001918
Igor Martins <iguinhoigor4@gmail.com>
2021-04-20 20:13:37 -0300

    feat: Finalizar o Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,10 @@ export function Header () {
     return (
         <header>
             <img src="/logo.svg" alt="Podcastr"/>
+
+            <p>O melhor para você ouvir, sempre</p>
+
+            <span>Qui, 8 Abril</span>
         </header>
     );
 }
